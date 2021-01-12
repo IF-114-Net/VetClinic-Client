a0468d102951e6c8fd7101d4cb8d7cb0825fa74b
Nick <yankivnick@gmail.com>
2021-01-12 16:36:50 +0200

    Fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppointmentComponent } from './modules/appointment/appointment/appointment.component';
 
-const routes: Routes = [
-  { path: 'appointments', component: AppointmentComponent },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
