422661e80e19ad21ed01d82659b7c21ef2169b37
Arsen <arsenyaremin@gmail.com>
2021-01-13 17:38:32 +0200

    Add header, footer, appointment button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule, LogLevel, OidcConfigService } from 'angular-auth-oidc-client';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { HttpClientModule } from '@angular/common/http';
+import { AuthConfigModule } from './auth/auth-config.module';
+import { MainNavComponent } from './shared/components/main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { AuthService } from './services/auth.service';
+import { HomeComponent } from './shared/components/home/home.component';
+import { LandingMakeAppointmentComponent } from './shared/components/landing-make-appointment/landing-make-appointment.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+
+export function configureAuth(oidcConfigService: OidcConfigService) {
+  return () =>
+    oidcConfigService.withConfig({
+      stsServer: 'https://localhost:5005',
+      redirectUrl: window.location.origin,
+      postLogoutRedirectUri: window.location.origin,
+      clientId: 'angular_client',
+      scope: 'openid profile ApiOne',
+      responseType: 'code',
+      //silentRenew: true,
+      //silentRenewUrl: `${window.location.origin}/silent-renew.html`,
+      logLevel: LogLevel.Debug,
+    });
+}
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MainNavComponent, HomeComponent, LandingMakeAppointmentComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AuthModule.forRoot(),
     BrowserAnimationsModule,
     MaterialModule,
-    HttpClientModule
+    HttpClientModule,
+    AuthConfigModule,
+    LayoutModule,
   ],
-  providers: [],
+  providers: [
+    OidcConfigService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: configureAuth,
+      deps: [OidcConfigService],
+      multi: true,
+    },
+    AuthService,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
