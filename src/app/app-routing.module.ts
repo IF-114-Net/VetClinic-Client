cedf31d45f8959aa1815358367d35b1cb15f5b29
BohdanYosypenko <idybp28@gmail.com>
2021-01-14 11:10:46 +0200

    add route to route file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DoctorListComponent } from './shared/components/doctor/doctor-list/doctor-list.component';
+import { DoctorPageComponent } from './shared/components/doctor/doctor-page/doctor-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'doctor',component:DoctorListComponent},  
+{path:'doctor/:id',component:DoctorPageComponent},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shared/components/doctor/doctor.module.ts
+++ src/app/shared/components/doctor/doctor.module.ts
@@ -1,24 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { DoctorListComponent } from './doctor-list/doctor-list.component';
 import { DoctorPageComponent } from './doctor-page/doctor-page.component';
 import { DoctorItemComponent } from './doctor-item/doctor-item.component';
-import { AppRoutingModule } from 'src/app/app-routing.module';
 import { MaterialModule } from 'src/app/material/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { DoctorDescriptionItemComponent } from './doctor-item/doctor-description-item/doctor-description-item.component';
-
-const appRoutes: Routes=[  
-  {path:'doctor',component:DoctorListComponent},  
-  {path:'doctor/:id',component:DoctorPageComponent},  
-]
 
 @NgModule({
   declarations: [DoctorListComponent, DoctorItemComponent, DoctorPageComponent, DoctorDescriptionItemComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot(appRoutes),
     MaterialModule
   ]
 })
