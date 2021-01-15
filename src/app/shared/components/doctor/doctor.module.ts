10fabac2323dc2b6d023b1a688eb04509cb13e56
BohdanYosypenko <idybp28@gmail.com>
2021-01-15 19:44:37 +0200

    Create Edit component and update doctor page

--- src/app/shared/components/doctor/doctor.module.ts
+++ src/app/shared/components/doctor/doctor.module.ts
@@ -5,12 +5,15 @@ import { DoctorItemComponent } from './doctor-item/doctor-item.component';
 import { MaterialModule } from 'src/app/material/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { DoctorDescriptionItemComponent } from './doctor-item/doctor-description-item/doctor-description-item.component';
+import { EditDoctorComponent } from './edit-doctor/edit-doctor.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [DoctorListComponent, DoctorItemComponent, DoctorPageComponent, DoctorDescriptionItemComponent],
+  declarations: [DoctorListComponent, DoctorItemComponent, DoctorPageComponent, DoctorDescriptionItemComponent, EditDoctorComponent],
   imports: [
     BrowserModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class DoctorModule { }
