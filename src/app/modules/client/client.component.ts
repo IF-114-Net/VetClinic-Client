2fb851df77f88553553410ada3617a95e9637901
BohdanYosypenko <idybp28@gmail.com>
2021-01-22 10:40:33 +0200

    add to cabinet

--- src/app/modules/client/client.component.ts
+++ src/app/modules/client/client.component.ts
@@ -12,37 +12,14 @@ import { ApiService } from 'src/app/services/api.service';
   templateUrl: './client.component.html',
   styleUrls: ['./client.component.css']
 })
-export class ClientComponent implements OnInit {
-  animals!:Animal[]  
-  animalType:AnimalType[]=new Array<AnimalType>();
-  id!:number;
-  activeAnimal!:number;
+export class ClientComponent implements OnInit {  
 
   constructor(private activatedRoute:ActivatedRoute,
     private apiService:ApiService) { }
 
-  ngOnInit(): void { 
-    
-    let params:Filter = {"clientId":"1"}   
-
-    this.apiService.getEntity('animals',params).subscribe((data: AnimalsData)=>{
-    this.animals=data.data; 
-    
-    for(let animal of this.animals)
-      {      
-      let id:number =parseInt(animal.animalTypeId) 
-      this.getAnimalTypeData(id)      
-      }          
-    })  
+  ngOnInit(): void {
   }
 
-  getAnimalTypeData(id:number){
-  this.apiService.getEntityById('animaltypes',id).subscribe((data: AnimalType)=>{
-    this.animalType.push(data)})
-  }
-
-  notActiveAnimal(active:any){
-  this.activeAnimal=active
-  }
+  
 
 }
